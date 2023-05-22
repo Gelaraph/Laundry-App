8394ecef8e0fa96ef0a08b53f07a73b426926bf1
Hammed Akinyemi <hakra911@gmail.com>
2023-05-22 09:57:49 +0100

    feat: added a app title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 
-const App = () => <div className="App" />
+const App = () => <div className="App">
+      <h1>
+      Laundry Website
+      </h1>
+</div>
 
 export default App;
